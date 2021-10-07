e2f42439b07f87a22ee3c30ec36314cfa100146e
Paloma Quiroz <paloma.quiroz@radiumrocket.com>
2021-10-07 17:12:04 -0300

    JS file with the resolutions for the 1st excercise created

---
+++ excercises/01_variables_and_operators.js
@@ -0,0 +1,20 @@
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var numberOne = 10;
+var numberTwo = 5;
+var sum = numberOne + numberTwo;
+console.log(sum);
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var firstWord = 'Eso';
+var secondWord = 'Tilín!';
+var phrase = firstWord + ' ' + secondWord;
+console.log(phrase);
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var firstPhrase = 'Hola Juan Carlos'
+var secondPhrase = 'Como estás'
+var phrasesLength = firstPhrase.length + secondPhrase.length;
+console.log(phrasesLength);
