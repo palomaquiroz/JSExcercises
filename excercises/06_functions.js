2e2bf3c4c97e7b5eedb75e5892fdedb70c226f4a
Paloma Quiroz <paloma.quiroz@radiumrocket.com>
2021-10-10 19:46:30 -0300

    Solved excercise 6 e

--- excercises/06_functions.js
+++ excercises/06_functions.js
@@ -1,17 +1,17 @@
 // Crear una función suma que reciba dos valores numéricos y retorne el resultado.
 // Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
 
-function sum(a, b) {
+function sum1(a, b) {
     var result = a + b;
     return result;
 }
 
-console.log(sum(1, 2));
+console.log(sum1(1, 2));
 
 // A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número,
 // mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 
-function sum(a, b) {
+function sum2(a, b) {
     if (isNaN(a)) {
         alert('Error: ' + a + ' is not a number');
         return NaN;
@@ -26,7 +26,7 @@ function sum(a, b) {
     }
 }
 
-console.log(sum(2, 'four'));
+console.log(sum2(2, 'four'));
 
 // Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
 
@@ -43,7 +43,19 @@ console.log(validateInteger(60));
 
 // A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros.
 // En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
-function sum(a, b) {
+
+function isInteger(a, b) {
+    if (!Number.isInteger(a)) {
+        alert('Error: ' + a + ' is not an integer');
+        return parseInt(a, 10);
+    }
+    else if (!Number.isInteger(b)) {
+        alert('Error: ' + b + ' is not a integer');
+        return parseInt(b, 10);
+    }
+}
+
+function sum3(a, b) {
     if (isNaN(a)) {
         alert('Error: ' + a + ' is not a number');
         return a;
@@ -52,13 +64,23 @@ function sum(a, b) {
         alert('Error: ' + b + ' is not a number');
         return b;
     }
-    else if (!Number.isInteger(a)) {
-        alert('Error: ' + a + ' is not an integer');
-        return parseInt(a, 10);
+    else {
+        isInteger(a, b);
     }
-    else if (!Number.isInteger(b)) {
-        alert('Error: ' + b + ' is not a integer');
-        return parseInt(b, 10);
+}
+
+console.log(sum3(2.9, 3));
+
+// Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
+
+function validateNumber(a, b) {
+    if (isNaN(a)) {
+        alert('Error: ' + a + ' is not a number');
+        return NaN;
+    }
+    else if (isNaN(b)) {
+        alert ('Error: ' + b + ' is not a number');
+        return NaN;
     }
     else {
         var result = a + b;
@@ -66,4 +88,9 @@ function sum(a, b) {
     }
 }
 
-console.log(sum(2.9, 3));
+function sum(a, b) {
+    validateNumber(a, b);
+    return result;
+}
+
+console.log(sum(3, 'cinco'));
